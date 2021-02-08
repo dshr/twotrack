dd3ef80b01f877285c2731e9202595548b7ddeb8
Desislav Hristov <hristov.desislav@gmail.com>
2021-02-08 21:26:15 +0300

    Initial Commit

---
+++ twotrack/twotrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  twotrackApp.swift
+//  twotrack
+//
+//  Created by Desislav Hristov on 08/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct twotrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
